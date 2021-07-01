b39d25f8700f6783aac2e143cf4eccd7242d71ae
yj1016904204 <yj1016904204@gmail.com>
2021-07-01 10:53:28 +0800

    cleanup

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+//vue3中配置项需要重新建个文件进行配置
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+        'views': '@/views',
+      }
+    }
+  }
+}
